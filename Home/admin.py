ddd847f09cb497fb0556d1c65288848296cf0c75
Hemananda Reddy <hemanandreddy3@gmail.com>
2022-12-01 12:55:14 +0530

    latest connit

--- Home/admin.py
+++ Home/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from Home.models import Orders
+from Home.models import wallet
 # Register your models here.
 admin.site.register(Orders)
+admin.site.register(wallet)
--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
@@ -13,4 +14,15 @@ class Orders(models.Model):
 
     def __str__(self):
         a = str(str(self.CustomerID)+"_Amount_Rs."+str(self.Buy_SellPrice*self.NumberOfShares)+"/-_"+self.ShareName+"_"+str(self.CustomerName)+"_"+str(self.OrderDate)+"_"+str(self.OrderTime))
+        return a 
+
+
+class wallet(models.Model):
+    money=models.IntegerField()
+    date = models.DateField()
+    time=models.TimeField()
+    upi_id=models.CharField(max_length=25)
+    Total_money=models.IntegerField()
+    def __str__(self):
+        a = str(str(self.money)+"__"+str(self.date)+"__"+str(self.time))
         return a 
